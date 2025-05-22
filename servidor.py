59379b18a94cd166190cb1a44d5e819071df5376
zelucasfc <joselucasfc87@gmail.com>
2025-05-21 21:01:00 -0400

    Update servidor.py

--- servidor.py
+++ servidor.py
@@ -37,5 +37,8 @@ def lock():
         return jsonify({"error": "Dispositivo não encontrado"}), 404
 
 
+import os
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=10000)
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port)
